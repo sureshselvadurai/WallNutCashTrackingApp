be0f7447b75700055ebc1b1b25b7e11deb8f8f2a
sureshrajaselvadurai <sureshrajaselvadurai@gmail.com>
2023-09-09 21:40:00 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WallNut Cash Tracking App"
+include(":app")
+ 
